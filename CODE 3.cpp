d1b5a69aad6f53b2265cf4e1c966cbef82f603a9
Waleedghostops5 <waleedrehan.ucp@gmail.com>
2025-04-19 17:50:51 +0500

    Update CODE 3.cpp

--- CODE 3.cpp
+++ CODE 3.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 using namespace std;
 
-// A CODE BY MUHAMMAD WALEED REHAN
-
 int myfunction(char x[8]);
-int main() 
+
+int main()
 {
-	char a[8];
-	for (int i = 0; i <= 7; i++)
-	{
-		cin >> a[i];
-	}
-	int number = myfunction(a);
-	cout << "TOTAL NUMBER OF VOVELS ARE: " << number;
-	return 0;
+    char a[8];
+    for (int i = 0; i < 8; i++)  
+    {
+        cin >> a[i];
+    }
+    int number = myfunction(a);
+    cout << "TOTAL NUMBER OF VOWELS ARE: " << number;
+    return 0;
 }
+
 int myfunction(char a[8])
 {
-	int num = 0;
-	for (int i = 0; i <= 7; i++) 
-	{
-		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u') {
-			num++;
-		}
-	}
-	return num;
+    int num = 0;
+    for (int i = 0; i < 8; i++)  
+    {
+        if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' ||
+            a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U') {
+            num++;
+        }
+    }
+    return num;
 }
