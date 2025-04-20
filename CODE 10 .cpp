0af68dcf9d4c1d39a758730552187a148509b52e
Waleedghostops5 <waleedrehan.ucp@gmail.com>
2025-04-20 20:56:16 +0500

    Add files via upload

---
+++ CODE 10 .cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// A CODE BY MUHAMMAD WALEED REHAN
+
+void ispalindrome(char str[])
+{
+	int len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+
+	}
+
+	cout << "Lenght is " << len << endl;
+
+	for (int i = 0;i < len / 2;i++)
+	{
+		if (str[i] != str[len - i - 1])
+		{
+			cout << "Not Palindrome " << endl;
+
+
+
+		}
+		else
+		{
+			cout << "Palindrome  Array";
+		}
+
+	}
+
+
+
+
+}
+int main()
+{
+	char s[100];
+	cout << "Enter a word";
+	cin >> s;
+	ispalindrome(s);
+
+
+
+
+}
